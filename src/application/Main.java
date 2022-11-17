def2fca396c51e25664ede9b85774f1e2950526d
CS219-user <CS219-user@10.0.0.45>
2022-11-17 21:52:55 +0000

    Added some UI that does not load currently but visually should be where I want it

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,22 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			
+			FXMLLoader loader = new FXMLLoader();
+			VBox root = loader.load(new FileInputStream("src/application/FinalProjectView.fxml"));
+			Scene scene = new Scene(root,600,600);
 				
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Calorie Calculator");
---
+++ src/application/ProjectViewController.java
@@ -0,0 +1,117 @@
+package application;
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class ProjectViewController {
+	
+	@FXML
+    private Label weightUnitLabel;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    private Label goalUnitLabel;
+
+    @FXML
+    private Button metricButton;
+
+    @FXML
+    private Label heightUnitLabel;
+
+    @FXML
+    private TextField timeTextField;
+
+    @FXML
+    private TextField goalTextField;
+
+    @FXML
+    private Label validLabel;
+
+    @FXML
+    private TextField ageTextField;
+
+    @FXML
+    private TextField weightTextField;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private TextField averageTextField;
+
+    @FXML
+    private Button imperialButton;
+
+    @FXML
+    private TextField heightTextField;
+
+    @FXML
+    private ChoiceBox<String> bodyTypeChoiceBox;
+
+    @FXML
+    private ChoiceBox<String> sexChoiceBox;        
+    
+    @FXML
+    void initialize() {		
+    	}
+    
+    @FXML
+    void imperialButtonClicked(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void metricButtonClicked(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void clearButtonClicked(ActionEvent event) {
+    	ageTextField.setText(null);
+    	heightTextField.setText(null);
+    	weightTextField.setText(null);
+    	goalTextField.setText(null);
+    	timeTextField.setText(null);
+    	averageTextField.setText(null);
+    	bodyTypeChoiceBox.getSelectionModel().select(-1);
+    	sexChoiceBox.getSelectionModel().select(-1);
+    }
+
+    @FXML
+    void enterButtonClicked(ActionEvent event) throws Exception {
+		
+    }
+    
+    private boolean isInt(String input) {
+		
+	    boolean x = true;
+	    try {
+	        Integer.parseInt(input);
+	    }catch (NumberFormatException e) {
+	        x = false;
+	    }
+	    return x;
+	}
+    
+    private boolean isDouble(String input) {
+	
+    	boolean x = true;
+    	try {
+    		Double.parseDouble(input);
+    	}catch (NumberFormatException e) {
+    		x = false;
+    	}
+    	return x;
+    }
+
+}
